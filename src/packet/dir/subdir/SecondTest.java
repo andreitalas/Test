99db56b1780f9ab26c870c3ab52ca38fb5679bc9
andreitalas <andreitalas@gmail.com>
2014-11-05 10:42:28 +0200

    test test test

--- src/packet/dir/subdir/SecondTest.java
+++ src/packet/dir/subdir/SecondTest.java
@@ -2,8 +2,10 @@ package packet.dir.subdir;
 
 /**
  * Created by Andrei on 05.11.2014.
+ *
+ * This is hust a test class
  */
-public class test2 {
+public class SecondTest {
     /**
      * This is just a test
      */
